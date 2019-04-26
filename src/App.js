802898a708efebb7c09f72b2e9d3b70abdf8ce72
zibi1993 <s.zygmunt1993@gmail.com>
2019-04-26 08:33:55 +0200

    create Logo and search box

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import './App.css';
-import './tachyons';
+
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <Navigation/> 
+        <Logo/>
+       <ImageLinkForm/>
+       {/* <FaceRecognition/>
+       */} 
       </div>
     );
   }
